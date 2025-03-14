953beae08f02ec91c044082e758bc5e94193aee2
SOMESHIITH <shivjaiswal51230@gmail.com>
2025-03-14 19:26:58 +0530

    completed all modules

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,11 +1,17 @@
+import os
 
+DATABASE_NAME = "sensor_fault_DB"
+COLLECTION_NAME = "sensor_fault"
 
+TARGET_COLUMN = "quality"
 url = "mongodb+srv://pwskills:pwskills@cluster0.mf0wr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 USERNAME = "pwskills"
 PASSWORD = "pwskills"
 
-DATABASE_NAME = "sensor_fault_DB"
-COLLECTION_NAME = "sensor_fault"
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
 
 "hyphen e . (-e .)" = "used to tell -e stands for editable mode and . (dot) refers to the current directory (which should contain a setup.py)"
+
+artifact_folder = "artifacts"
